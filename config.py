1b0cc34fde3a6fda1884e8e46c1729dc14d08031
karpoker <126786427+karpoker@users.noreply.github.com>
2023-06-07 14:44:14 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+from datetime import datetime, timedelta
+
+token = '6173073911:AAHYrFOg5Z14cJBtiUtUAFEt0zCIDac7UxU' # токен бота
+
+time = timedelta(day=1) # задержка на измерение (seconds, minutes, day)
